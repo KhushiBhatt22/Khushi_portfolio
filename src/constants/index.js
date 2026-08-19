3ae8dd3dda0e664602700f4f853ded6e46515634
Khushi Bhatt <khushi.bhatt0405@gmail.com>
2026-08-19 00:50:06 -0700

    Update few content

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,13 +24,6 @@ const words = [
   { text: "Code", imgPath: "/images/code.svg" },
 ];
 
-const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
-  { value: 90, suffix: "%", label: "Client Retention Rate" },
-];
-
 const logoIconsList = [
   {
     imgPath: "/images/logos/company-logo-1.png",
@@ -263,7 +256,6 @@ export {
   words,
   abilities,
   logoIconsList,
-  counterItems,
   expCards,
   expLogos,
   testimonials,
