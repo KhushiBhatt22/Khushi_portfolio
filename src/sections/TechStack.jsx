ca45a3779d643f03c9489982c58f002f9a70ff4e
Khushi Bhatt <khushi.bhatt0405@gmail.com>
2026-08-20 18:24:33 -0700

    Update the header

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -59,7 +59,7 @@ const TechStack = () => {
   return (
     <div id="skills" className="flex-center section-padding">
       <div className="w-full h-full md:px-10 px-5">
-        <TitleHeader title="Skills" sub="💡 My Core Stack" />
+        <TitleHeader title="Skills" sub="My Core Stack" />
 
         <div
           className="skill-cards-grid"
