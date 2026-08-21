fd391d7f260aece523712e3a88bd0a9b0a49f48b
Khushi Bhatt <khushi.bhatt0405@gmail.com>
2026-08-20 18:23:45 -0700

    Modified the experince content

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -26,7 +26,7 @@ const timelineEntries = [
 ];
 
 const Experience = () => (
-  <section id="experience" className="flex-center md:mt-40 mt-20 section-padding xl:px-0">
+  <section id="experience" className="flex-center section-padding xl:px-0">
     <div className="w-full h-full md:px-20 px-5">
       <TitleHeader title="Career Timeline" sub="My journey so far" />
       <div className="career-timeline mt-20">
