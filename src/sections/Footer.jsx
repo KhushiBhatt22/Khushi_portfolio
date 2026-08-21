2eab893d34c80d9a37fdb9a3f8b69973f23daff9
Khushi Bhatt <khushi.bhatt0405@gmail.com>
2026-08-20 18:23:58 -0700

    Add footer content

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,23 +1,29 @@
-import { socialImgs } from "../constants";
-
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <div className="flex flex-col justify-center">
-          <p>Terms & Conditions</p>
-        </div>
+        <p>&copy; 2026 Khushi Bhatt</p>
         <div className="socials">
-          {socialImgs.map((socialImg, index) => (
-            <div key={index} className="icon">
-              <img src={socialImg.imgPath} alt="social icon" />
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-col justify-center">
-          <p className="text-center md:text-end">
-            © {new Date().getFullYear()} Adrian Hajdin. All rights reserved.
-          </p>
+          <a
+            href="https://www.linkedin.com/in/khushi-bhatt-1b49a0229/"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Khushi Bhatt on LinkedIn"
+          >
+            <svg viewBox="0 0 24 24" aria-hidden="true">
+              <path d="M5.1 3.5A2.1 2.1 0 1 1 1 3.5a2.1 2.1 0 0 1 4.1 0ZM1.4 8h3.5v11H1.4V8Zm5.7 0h3.4v1.5h.1c.5-.9 1.7-1.9 3.6-1.9 3.8 0 4.5 2.5 4.5 5.8V19h-3.5v-5c0-1.2 0-2.9-1.8-2.9s-2 1.4-2 2.8V19H7.1V8Z" />
+            </svg>
+          </a>
+          <a
+            href="https://github.com/KhushiBhatt22"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Khushi Bhatt on GitHub"
+          >
+            <svg viewBox="0 0 24 24" aria-hidden="true">
+              <path d="M12 1.5a10.5 10.5 0 0 0-3.3 20.5c.5.1.7-.2.7-.5v-2c-2.8.6-3.4-1.2-3.4-1.2-.5-1.2-1.1-1.5-1.1-1.5-.9-.6.1-.6.1-.6 1 .1 1.5 1 1.5 1 .9 1.5 2.3 1.1 2.9.8.1-.6.4-1.1.7-1.3-2.2-.3-4.5-1.1-4.5-4.9 0-1.1.4-2 1-2.7-.1-.3-.4-1.3.1-2.7 0 0 .9-.3 2.8 1a9.7 9.7 0 0 1 5.1 0c1.9-1.3 2.8-1 2.8-1 .5 1.4.2 2.4.1 2.7.7.7 1 1.6 1 2.7 0 3.8-2.3 4.6-4.5 4.9.4.3.7.9.7 1.8v2.7c0 .3.2.6.7.5A10.5 10.5 0 0 0 12 1.5Z" />
+            </svg>
+          </a>
         </div>
       </div>
     </footer>
